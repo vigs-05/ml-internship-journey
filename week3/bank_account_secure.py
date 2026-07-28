8c75c4f1983683494f3264789bc80aa0dbac1cce
Vignesh Jadhav <jvighnesh8@gmail.com>
2026-07-28 16:43:33 +0530

    Create bank_account_secure.py

---
+++ week3/bank_account_secure.py
@@ -0,0 +1,23 @@
+class BankAccount:
+    
+    def __init__(self):
+        self.__balance = 0 # Private variable or instance
+        
+    # Getter ie to get balance
+    @property
+    def balance(self):
+        return self.__balance
+    
+    # Setter to set or modify the private data variable or instance
+    @balance.setter
+    def balance(self,amount):
+        if(amount < 0):
+            print("ERROR : Cannot set negative balance !!!")
+        else:
+            self.__balance = amount
+
+acc = BankAccount()
+print(acc.balance)
+acc.balance = 1000
+print(acc.balance)
+acc.balance =
